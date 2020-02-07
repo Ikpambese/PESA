8992ddde1ea9b24f1c9ca6c1bcdab0d309e5d393
Maverick <45435017+demicoder@users.noreply.github.com>
2020-02-07 22:10:55 +0100

    added about content

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AboutScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('about'), backgroundColor: Colors.blue),
-
-      body: Center(child: Text('About this app...'),),
+      body: Center(
+        child: Text(
+          'Development of Cross-Platform Dependent Multi-Choice Pre-Exam Study Question System(A case study of GST and Departmental Courses)',
+          textAlign: TextAlign.center,
+        ), 
+      ),
     );
   }
 }
